db5a5122b5e09a87d6b2a6a257767b3fd151e6dc
Aleksander <asava@sibmail.com>
2023-04-26 18:14:06 +0700

    конвертировали в mp3

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
 import config from 'config'
 import { ogg } from './ogg.js'
+import { openai } from './openai.js'
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
@@ -10,9 +11,12 @@ bot.on(message('voice'), async (ctx) => {
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
         const userId = String(ctx.message.from.id)
         const oggPath = await ogg.create(link.href, userId)
-        const mp3Path = await ogg.toMp3(oggPath)
-        console.log(oggPath)
-        await ctx.reply(JSON.stringify(link, null, 2))
+        const mp3Path = await ogg.toMp3(oggPath, userId) // конвертировали в mp3
+
+        const text = await openai.transcription(mp3Path)
+        const response = await openai.chat(text)
+        // console.log(oggPath)
+        await ctx.reply(mp3Path)
     } catch (e) {
         console.log('Error while voice massage', e.message)
     }

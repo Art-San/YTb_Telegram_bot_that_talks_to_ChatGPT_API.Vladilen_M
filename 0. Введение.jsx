60de802c041ffba8363bc64dee05457607dbbb69
Aleksander <asava@sibmail.com>
2023-04-26 14:33:14 +0700

    31:28 min

---
+++ 0. Введение.jsx
@@ -0,0 +1,30 @@
+// npm init -y // node.js
+
+// npm i -D nodemon cross-env
+
+// {
+//     "name": "ytb_telegram_bot_that_talks_to_chatgpt_api.vladilen_m",
+//     "version": "1.0.0",
+//     "description": "",
+//     "main": "index.js",
+//     "type": "module",  // для использования современого синтаксиса ИМПОРТА и ЭКСПОРТА
+//     "scripts": {
+//         "dev": "nodemon ./src/main.js",
+//         "start": "node ./src/main.js"
+//     },
+//     "keywords": [],
+//     "author": "",
+//     "license": "ISC",
+//     "dependencies": {
+//         "cross-env": "^7.0.3",
+//         "nodemon": "^2.0.22"
+//     }
+// }
+
+// Базовая настройка готова
+// Далее
+
+// npm i telegraf
+// npm i config
+
+// npm i axios fluent-ffmpeg @ffmpeg-installer/ffmpeg

dde66d416820d2605a1129db938a1e40a456e3ad
Aleksander <asava@sibmail.com>
2023-04-27 23:01:55 +0700

    у огг нет айди

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
 import config from 'config'
 import { ogg } from './ogg.js'
-import { openai } from './openai.js'
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
@@ -11,12 +10,9 @@ bot.on(message('voice'), async (ctx) => {
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
         const userId = String(ctx.message.from.id)
         const oggPath = await ogg.create(link.href, userId)
-        const mp3Path = await ogg.toMp3(oggPath, userId) // конвертировали в mp3
-
-        const text = await openai.transcription(mp3Path)
-        const response = await openai.chat(text)
-        // console.log(oggPath)
-        await ctx.reply(mp3Path)
+        const mp3Path = await ogg.toMp3(oggPath)
+        console.log(oggPath)
+        await ctx.reply(JSON.stringify(link, null, 2))
     } catch (e) {
         console.log('Error while voice massage', e.message)
     }
@@ -24,6 +20,7 @@ bot.on(message('voice'), async (ctx) => {
 
 bot.command('start', async (ctx) => {
     await ctx.reply(JSON.stringify(ctx.message, null, 2))
+    // console.log(ctx.message)
 })
 
 bot.launch()

a3e309497e284b1e5d90452f71d89d5c02002e4b
Aleksander <asava@sibmail.com>
2023-04-27 23:26:40 +0700

    ok

--- src/main.js
+++ src/main.js
@@ -1,3 +1,88 @@
+// import { Telegraf, session } from 'telegraf'
+// import { message } from 'telegraf/filters'
+// import { code } from 'telegraf/format'
+// import config from 'config'
+// import { ogg } from './ogg.js'
+// import { openai } from './openai.js'
+
+// console.log(config.get('TEST_ENV'))
+
+// const arrId = [466220524, 5016904767, 721836748]
+// // const arrId = [466220524, 5016904767, 721836748]
+
+// const INITIAL_SESSION = {
+//     messages: []
+// }
+
+// const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+
+// bot.use(session())
+
+// bot.command('new', async (ctx) => {
+//     ctx.session = INITIAL_SESSION
+//     await ctx.reply('Жду вашего голосового или текстового сообщения')
+// })
+
+// bot.command('start', async (ctx) => {
+//     ctx.session = INITIAL_SESSION
+//     await ctx.reply('Жду вашего голосового или текстового сообщения')
+// })
+
+// bot.on(message('voice'), async (ctx) => {
+//     ctx.session ??= INITIAL_SESSION
+//     try {
+//         await ctx.reply(code('Сообщение принято Жду ответ от сервера...'))
+//         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+//         const userId = String(ctx.message.from.id)
+//         const oggPath = await ogg.create(link.href, userId)
+//         const mp3Path = await ogg.toMp3(oggPath, userId)
+
+//         const text = await openai.transcription(mp3Path)
+//         await ctx.reply(code(`Ваш запрос: ${text}`))
+
+//         ctx.session.messages.push({ role: openai.roles.USER, content: text })
+
+//         const response = await openai.chat(ctx.session.messages)
+
+//         ctx.session.messages.push({
+//             role: openai.roles.ASSISTANT,
+//             content: response.content
+//         })
+
+//         await ctx.reply(response.content)
+//     } catch (e) {
+//         console.log('Error while voice massage', e.message)
+//     }
+// })
+
+// bot.on(message('text'), async (ctx) => {
+//     ctx.session ??= INITIAL_SESSION
+//     try {
+//         await ctx.reply(code('Сообщение принято Жду ответ от сервера...'))
+
+//         ctx.session.messages.push({
+//             role: openai.roles.USER,
+//             content: ctx.message.text
+//         })
+
+//         const response = await openai.chat(ctx.session.messages)
+
+//         ctx.session.messages.push({
+//             role: openai.roles.ASSISTANT,
+//             content: response.content
+//         })
+
+//         await ctx.reply(response.content)
+//     } catch (e) {
+//         console.log('Error while text massage', e.message)
+//     }
+// })
+
+// bot.launch()
+
+// process.prependOnceListener('SIGINT', () => bot.stop('SIGINT'))
+// process.prependOnceListener('SIGTERM', () => bot.stop('SIGTERM'))
+
 import { Telegraf, session } from 'telegraf'
 import { message } from 'telegraf/filters'
 import { code } from 'telegraf/format'
@@ -8,7 +93,6 @@ import { openai } from './openai.js'
 console.log(config.get('TEST_ENV'))
 
 const arrId = [466220524, 5016904767, 721836748]
-// const arrId = [466220524, 5016904767, 721836748]
 
 const INITIAL_SESSION = {
     messages: []
@@ -34,6 +118,12 @@ bot.on(message('voice'), async (ctx) => {
         await ctx.reply(code('Сообщение принято Жду ответ от сервера...'))
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
         const userId = String(ctx.message.from.id)
+
+        if (!arrId.includes(userId)) {
+            await ctx.reply('У вас нет доступа')
+            return
+        }
+
         const oggPath = await ogg.create(link.href, userId)
         const mp3Path = await ogg.toMp3(oggPath, userId)
 
@@ -59,6 +149,12 @@ bot.on(message('text'), async (ctx) => {
     ctx.session ??= INITIAL_SESSION
     try {
         await ctx.reply(code('Сообщение принято Жду ответ от сервера...'))
+
+        const userId = String(ctx.message.from.id)
+        if (!arrId.includes(userId)) {
+            await ctx.reply('У вас нет доступа')
+            return
+        }
 
         ctx.session.messages.push({
             role: openai.roles.USER,
